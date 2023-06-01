8d20f7395b7cf62f0d787f11b3aa0c3b6626a9c7
joris-landaret <joris.landaret@laplateforme.io>
2023-06-01 17:15:12 +0200

    page html dessin pas à pas avec designe css

---
+++ dessin_pas_a_pas.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/css/dessin_pas_a_pas.css">
+    <title>Dessin pas à pas</title>
+</head>
+
+<body>
+    <?php
+    //require_once("header.html");
+    ?>
+    <main>
+        <h1>Desssin pas à pas</h1>
+        <div>
+            <a href="">PAYSAGES</a>
+            <a href="" id="rectangle">PERSONNAGES</a>
+            <a href="">ANIMAUX
+        </div></a>
+        </div>
+        <div id="ligne2">
+            <a href="">SPORTS</a>
+            <a href="">MOYENS DE TRANSPORT</a>
+        </div>
+    </main>
+</body>
+
+</html>
