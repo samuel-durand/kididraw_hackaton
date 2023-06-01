ba02c5729adc95aea200395d323d54f938513b1e
samuel-durand <114388132+samuel-durand@users.noreply.github.com>
2023-06-01 17:28:20 +0200

    update

---
+++ src/php/leçons.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Leçons</title>
+</head>
+<body>
+    <section>
+        <main>
+            <h1 class="title">leçons</h1>
+            <div>
+                <h3>Ces vidéos t'aiderons à réaliser tes missions !</h3>
+                </div>
+
+        </main>
+        </section>
+
+        <div class="main">
+      <div class="column1">
+      </div>
+      <div class="column2">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      </div>
+      <div id="bottom">
+      <h1>Les proportions</h1>
+      <h6>Regarde cette vidéo dessine</h6>
+      </div>
+    </div>
+    <div class="button-container">
+        <button>Mon bouton</button>
+    </div>
+
+    <div class="main">
+      <div class="column1">
+        <h2></h2>
+      </div>
+      <div class="column2">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      </div>
+        <div id="bottom">
+            
+        <h1>Les directions</h1>
+        <h6>Regarde cette vidéo dessine</h6>
+        </div>
+
+    </div>
+    <div class="button-container">
+        <button>Mon bouton</button>
+    </div>
+
+
+    <div class="main">
+      <div class="column1">
+        <h2></h2>
+      </div>
+      <div class="column2">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      </div>
+        <div id="bottom">
+            
+        <h1>Les formes simple</h1>
+        <h6>Regarde cette vidéo dessine</h6>
+        </div>
+
+    </div>
+    <div class="button-container">
+        <button>Mon bouton</button>
+    </div>
+
+
+    <div class="main">
+      <div class="column1">
+        <h2></h2>
+      </div>
+      <div class="column2">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      </div>
+        <div id="bottom">
+            
+        <h1>Les positions</h1>
+        <h6>Regarde cette vidéo dessine</h6>
+        </div>
+
+    </div>
+    <div class="button-container">
+        <button>Mon bouton</button>
+    </div>
+
+    <div class="main">
+      <div class="column1">
+        <h2></h2>
+      </div>
+      <div class="column2">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      </div>
+        <div id="bottom">
+            
+        <h1>Les valeurs</h1>
+        <h6>Regarde cette vidéo dessine</h6>
+        </div>
+
+    </div>
+    <div class="button-container">
+        <button>Mon bouton</button>
+    </div>
+
+
+</body>
+</html>
